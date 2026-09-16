5ce9d32861bb4fecaf69c6e79993e7296af73dd5
hernandario1957 <hernandedurango@gmail.com>
2026-09-16 09:46:45 -0500

    Integra nueva interfaz LexDigitalHD y corrige layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,13 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://lexdigitalhd.com',
   integrations: [
-    // Activamos la integración de Tailwind con su configuración por defecto
-    tailwind(),
-    // sitemap(), // (Se mantiene comentado hasta que quieras activar el mapa del sitio)
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    // sitemap(),
   ],
-  output: 'static'
-  // Eliminamos la regla de "inlineStylesheets" para que Cloudflare maneje los archivos CSS de forma nativa
+  output: 'static',
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
